b5c942ae841e87ebb0d51cb2f27bc366cb469eca
임소정 <imsojeong@sojeong-ui-MacBookAir.local>
2026-03-25 10:42:33 +0900

    Initial commit for deployment

---
+++ next.config.mjs
@@ -0,0 +1,10 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  output: 'export',
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  
